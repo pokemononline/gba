2cf5a1587c274a8bcd402a7a75019977f9b60368
jsemu <jsemu@jsemu.org>
2014-06-29 07:07:18 -0400

    adding a game

--- user_scripts/IodineGBACoreGlueCode.js
+++ user_scripts/IodineGBACoreGlueCode.js
@@ -20,6 +20,7 @@ var games = {
     "alienhominid":"Alien Hominid",
     "bubblebobble":"Bubble Bobble",
     "gamewatch4":"Game & Watch Gallery 4",
+    "gunstar_super_heroes":"Gunstar Super Heroes",
     "kirbymirror":"Kirby & The Amazing Mirror",
     "kirbynightmare":"Kirby: Nightmare in Dreamland",
     "marioparty":"Mario Party Advance",
